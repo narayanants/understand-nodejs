dd1efd9ae908ae3be8db722a1112586401b91e36
Narayanan Ts <tsnarayanan7@gmail.com>
2017-03-03 10:30:50 +0530

    removed v3 and v4

--- 4 Modules, Exports, and Require/Code Samples/D11_ModulePatterns/Starter_Recap/v3.js
+++
@@ -1,8 +0,0 @@
-function Version(){
-    this.greeting  = 'Welcome to version 3';
-    this.greet = function(){
-        console.log(this.greeting);
-    }
-}
-
-module.exports = new Version();
--- 4 Modules, Exports, and Require/Code Samples/D11_ModulePatterns/Starter_Recap/v4.js
+++
@@ -1,8 +0,0 @@
-function Version(){
-    this.greeting  = 'Welcome to version 4';
-    this.greet = function(){
-        console.log(this.greeting);
-    }
-}
-
-module.exports = Version;
