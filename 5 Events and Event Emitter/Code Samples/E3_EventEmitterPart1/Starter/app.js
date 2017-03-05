b4ccb75f261b5d6d75411dce643bd9115c2fa339
Narayanan Ts <tsnarayanan7@gmail.com>
2017-03-05 09:35:37 +0530

    removed comments

--- 5 Events and Event Emitter/Code Samples/E3_EventEmitterPart1/Starter/app.js
+++ 5 Events and Event Emitter/Code Samples/E3_EventEmitterPart1/Starter/app.js
@@ -1,18 +1,3 @@
-// var Emitter = require('./emitter');
-// var emtr = new Emitter();
-
-// emtr.on('greet',function(){
-// 	console.log('Greeted for 1st time!');
-// });
-
-// emtr.on('greet',function(){
-// 	console.log('Greeted for 2nd time!');
-// });
-
-// console.log('Hello');
-// emtr.emit('greet');
-
-
 var Emitter = require('./emitter');
 var emtr = new Emitter();
 
