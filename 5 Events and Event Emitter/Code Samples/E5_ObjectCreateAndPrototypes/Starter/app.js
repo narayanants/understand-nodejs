999edceded1a2a6abe542ec634d9f81d5a69cc33
Narayanan Ts <tsnarayanan7@gmail.com>
2017-03-05 10:30:40 +0530

    changes to app

--- 5 Events and Event Emitter/Code Samples/E5_ObjectCreateAndPrototypes/Starter/app.js
+++ 5 Events and Event Emitter/Code Samples/E5_ObjectCreateAndPrototypes/Starter/app.js
@@ -1,39 +1,22 @@
-// var person = {
-//     firstname: '',
-//     lastname: '',
-//     greet:function(){
-//         return this.firstname + ' ' + this.lastname;
-//     }
-// }
-
-
-// var john = Object.create(person);
-// john.firstname = 'John';
-// john.lastname = 'Doe';
-
-
-// var jane = Object.create(person);
-// jane.firstname = 'Jane';
-// jane.lastname = 'Doe';
-// console.log(john.greet());
-// console.log(jane.greet());
-
 var person = {
-	firstname:'Narayanan',
-	lastname:'Ts',
-	greet:function(){
-		return this.firstname + ' ' + this.lastname;
-	}
+    firstname: '',
+    lastname: '',
+    greet:function(){
+        return this.firstname + ' ' + this.lastname;
+    }
 }
+
 
 var john = Object.create(person);
 john.firstname = 'John';
 john.lastname = 'Doe';
-console.log(john.greet());
 
-//Jane
 
 var jane = Object.create(person);
 jane.firstname = 'Jane';
 jane.lastname = 'Doe';
+console.log(john.greet());
 console.log(jane.greet());
+
+
+
