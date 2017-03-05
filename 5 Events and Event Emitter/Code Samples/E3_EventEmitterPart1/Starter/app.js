13c853abaa2efade13fcb9d6e62cbfe66bb46427
Narayanan Ts <tsnarayanan7@gmail.com>
2017-03-05 09:25:18 +0530

    added app.js

--- 5 Events and Event Emitter/Code Samples/E3_EventEmitterPart1/Starter/app.js
+++ 5 Events and Event Emitter/Code Samples/E3_EventEmitterPart1/Starter/app.js
@@ -1,12 +1,27 @@
+// var Emitter = require('./emitter');
+// var emtr = new Emitter();
+
+// emtr.on('greet',function(){
+// 	console.log('Greeted for 1st time!');
+// });
+
+// emtr.on('greet',function(){
+// 	console.log('Greeted for 2nd time!');
+// });
+
+// console.log('Hello');
+// emtr.emit('greet');
+
+
 var Emitter = require('./emitter');
 var emtr = new Emitter();
 
 emtr.on('greet',function(){
-	console.log('Greeted for 1st time!');
-})
+	console.log('Hello world!');
+});	
 
 emtr.on('greet',function(){
-	console.log('Greeted for 2nd time!');
+	console.log('Hello world for 2nd time');
 });
 
 emtr.emit('greet');
