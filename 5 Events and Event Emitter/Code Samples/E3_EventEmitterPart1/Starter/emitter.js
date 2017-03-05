9421fb500feb1b348b092e37c16b0f2ebe0ff233
Narayanan Ts <tsnarayanan7@gmail.com>
2017-03-05 09:37:15 +0530

    removed comments

--- 5 Events and Event Emitter/Code Samples/E3_EventEmitterPart1/Starter/emitter.js
+++ 5 Events and Event Emitter/Code Samples/E3_EventEmitterPart1/Starter/emitter.js
@@ -1,39 +1,3 @@
-// function Emitter(){
-// 	this.events = {};
-// }
-
-// Emitter.prototype.on = function(type,listener){
-// 	this.events.type = this.events.type || [];
-// 	this.events.type.push(listener);
-// }
-
-// Emitter.prototype.emit = function(type){
-// 	if(this.events.type){
-// 		this.events.type.forEach(function(listener){
-// 			listener();
-// 		});
-// 	}
-// }
-
-// module.exports = Emitter;
-// function Emitter(){
-// 	this.events = {};
-// }
-
-// Emitter.prototype.on = function (type,listener) {
-// 	this.events.type = this.events.type || [];
-// 	this.events.type.push(listener);
-// };
-// Emitter.prototype.emit = function(type){
-// 	if(this.events.type){
-// 		this.events.type.forEach(function(listener){
-// 			listener();
-// 		});
-// 	}
-// };
-
-// module.exports = Emitter;
-
 function Emitter(){
 	this.events = {};
 }
